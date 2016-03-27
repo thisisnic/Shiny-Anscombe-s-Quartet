f33ee8c1d295d45da04104ddc5e7e3d77ebdbabd
thisisnic <thisisnic@gmail.com>
2016-03-27 15:31:47 +0100

    Updated comments

--- server.R
+++ server.R
@@ -1,24 +1,14 @@
 library(shiny)
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-  
-  # Expression that generates a histogram. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
-  
-  # Because we've got this as an attribute of output, we can just refer to it as plotOutput("distPlot") in the ui file
+
+  # a reactive plot
   output$distPlot <- renderPlot({
     
-    # x is a vector of numbers
-    
-    # get the relevant columns of the dataset
+    # get the appropriate values using the radio input
     x = anscombe[,c(as.numeric(input$radio),as.numeric(input$radio)+4)]
   
-    # draw the histogram with the specified number of bins
+    # plot the selected values
     plot(x, xlim=c(3,20), ylim=c(3,20), type="p", pch=16)
     
   })
