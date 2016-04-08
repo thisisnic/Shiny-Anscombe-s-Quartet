a5f5a6e4516bee32b57496f33dfa9a114e27d67e
thisisnic <thisisnic@gmail.com>
2016-04-08 16:47:47 +0100

    Added tables, properties and various minor adjustments

--- server.R
+++ server.R
@@ -1,15 +1,52 @@
 library(shiny)
 
 shinyServer(function(input, output) {
-
+  
+  
   # a reactive plot
   output$distPlot <- renderPlot({
     
     # get the appropriate values using the radio input
+    # the +4 refers to the fact that the corresponding x and y columns are 4 columns apart
     x = anscombe[,c(as.numeric(input$radio),as.numeric(input$radio)+4)]
-  
-    # plot the selected values
-    plot(x, xlim=c(3,20), ylim=c(3,20), type="p", pch=16)
     
+    # plot the selected values
+    # the xlim and ylim have been set here manually so all plots are the same size and do not crop any data
+    plot(x, xlim=c(3,20), ylim=c(3,15), type="p", pch=16, main=paste("Dataset ",as.numeric(input$radio)))
   })
+  
+  output$ans_table <- renderTable({
+    anscombe[,c(as.numeric(input$radio),as.numeric(input$radio)+4)] 
+  }
+  )
+  
+  output$data_meanx <- renderText({
+    number = as.numeric(input$radio)
+    mean(anscombe[,number])
+  })
+  
+  output$data_meany <- renderText({
+    number = as.numeric(input$radio)
+    mean(anscombe[,number+4])
+  })
+  
+  output$dataset_number<-renderText({
+    paste("Dataset ", as.numeric(input$radio))
+  })
+  
+  output$data_varx <- renderText({
+    number = as.numeric(input$radio)
+    var(anscombe[,number])
+  })
+  
+  output$data_vary <- renderText({
+    number = as.numeric(input$radio)
+    var(anscombe[,number+4])
+  })
+  
+  output$correlation <- renderText({
+    number = as.numeric(input$radio)
+    cor(anscombe[,number],anscombe[,number+4])
+  })
+  
 })
--- ui.R
+++ ui.R
@@ -19,12 +19,35 @@ shinyUI(fluidPage(
                   
     ),
     
-    
     mainPanel(
       
       tabsetPanel(
-        tabPanel("Plot", plotOutput("distPlot")), 
-        tabPanel("Description", p("Ancombe's Quartet is a group of 4 datasets constructed in 1973 by Francis Anscombe.  They are commonly used to emphasise the importance of exploratory anlayses."),  p("  Although these datasets all have identical or very similar means of x and y, variances of x and y, and correlations between x and y, when we plot them we can see that they show distinctly different trends in the data."))
+        tabPanel("Description",
+                 p("Ancombe's Quartet is a group of 4 datasets constructed in 1973 by Francis Anscombe.
+                   They are commonly used to emphasise the importance of exploratory anlayses."),
+                 p("Although these datasets all have identical or very similar means of x and y, 
+                   variances of x and y, and correlations between x and y, when we plot them we
+                   can see that they show distinctly different trends in the data."),
+                 p(
+                   strong("Toggle through the different datasets on the left and select a tab 
+                          to the right to explore the datasets")
+                 )
+                ),
+        tabPanel("View Raw Data", tableOutput("ans_table")),
+        tabPanel("View Data Properties",
+                 
+                 # Although the sidebar shows the number, reflected here so the user can see that...
+                 # ...something is being updated.
+                 p(h3(textOutput("dataset_number"))),
+                 p(strong("Mean (x):", textOutput("data_meanx"))),
+                 p(strong("Mean (y):", textOutput("data_meany"))),
+                 p(strong("Variance (x):", textOutput("data_varx"))),
+                 p(strong("Variance (y):", textOutput("data_vary"))),
+                 p(strong("Correlation between x and y:", textOutput("correlation")))
+                 
+                 ),
+        tabPanel("Plot Data", plotOutput("distPlot"))
+        
       )
       
     )
