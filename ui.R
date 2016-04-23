1c8e94ab83813af38165abad4aa126d95a643b19
thisisnic <thisisnic@gmail.com>
2016-04-23 23:54:00 +0100

    Added comments around conditional panels and tidied code

--- ui.R
+++ ui.R
@@ -7,14 +7,10 @@ shinyUI(fluidPage(
   
   sidebarLayout(
     
-    
-    
-    
-    
     sidebarPanel(
       
       # use conditional panels so only display the dataset selector on relevant tabs
-      
+      # if the user is not on the 'description' tab, show the dataset selector
       conditionalPanel(
         'input.whichTab != "desc"',
         
@@ -27,6 +23,7 @@ shinyUI(fluidPage(
         )
       ),
       
+      # if the user is on the 'description' tab, show some help text
       conditionalPanel(
         'input.whichTab === "desc"',
         helpText("Select a tab to the right to explore the datasets")
@@ -38,29 +35,28 @@ shinyUI(fluidPage(
       
       tabsetPanel(
         id="whichTab",
+        
         tabPanel("Description",
                  p("Ancombe's Quartet is a group of 4 datasets constructed in 1973 by Francis Anscombe.
                    They are commonly used to emphasise the importance of exploratory anlayses."),
                  p("Although these datasets all have identical or very similar means of x and y, 
                    variances of x and y, and correlations between x and y, when we plot them we
                    can see that they show distinctly different trends in the data."),
-                 
                  value="desc"),
+        
         tabPanel("View Raw Data", tableOutput("ans_table"), value="raw"),
+        
         tabPanel("View Data Properties",
                  
                  # Although the sidebar already shows the number, it's duplicated... 
                  # ...here so the user can see that something is being updated.
-                 
-                 
                  p(h3(textOutput("dataset_number"))),
                  p(tableOutput("properties")),
-                 
                  value="prop"),
+        
         tabPanel("Plot Data", plotOutput("distPlot"), value="plot")
         
       )
-      
     )
   )
 )
